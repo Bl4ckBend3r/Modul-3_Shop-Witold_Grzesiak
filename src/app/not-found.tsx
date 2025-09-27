d3d2d29f6881af144b97566854d99e66c9054485
Bl4ckBend3r <witold.g83@gmail.com>
2025-09-27 02:20:54 +0200

    fix: ensure not-found is a server component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-[#1A1A1A] text-white">
       <h1 className="text-5xl font-bold mb-4">404</h1>
-      <p className="text-lg mb-8">Strona nie została znaleziona</p>
+      <p className="text-lg mb-6">Strona nie została znaleziona.</p>
       <a
         href="/"
         className="rounded-md bg-[#F29145] px-6 py-2 text-black font-medium hover:bg-[#ee701d]"
